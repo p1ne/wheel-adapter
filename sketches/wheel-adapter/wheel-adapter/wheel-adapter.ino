648f994a7f2b6eb55453dede034f4ff383db5db9
p1ne <nnalutin@gmail.com>
2017-08-30 11:09:43 +0300

    Completely rewritten wheel adapter code to make it clearer. The only current problem - it skips track and only then starts to ffwd/rev inside the track. To be fixed.

--- sketches/wheel-adapter/wheel-adapter/wheel-adapter.ino
+++ sketches/wheel-adapter/wheel-adapter/wheel-adapter.ino
@@ -4,12 +4,6 @@
 
 #include "Tablet.h"
 
-int analogPin=3;    //пин для подключения кнопок
-int data;
-unsigned long flag = 0;
-unsigned long eventTime = 0;
-unsigned long pauseMillis = 0;
-
 // Constant values for 1K resistor
 
 #define NOBUTTON 484
@@ -19,129 +13,112 @@ unsigned long pauseMillis = 0;
 #define VOLUP 913
 #define VOLDOWN 972
 
-#define TOLERANCE 10
+uint8_t analogPin=3;
 
-boolean between(int value, int a, int b)
-{
-  if ( (value > a) && (value < b) ) return 1;
-  else return 0;
-}
+class WheelKeyEvent {
+protected:
+  uint16_t tolerance;
+  uint16_t resistance;
+  uint32_t timestamp;
 
-boolean tolerance(int value, int expected)
+public:
+  void set(const uint16_t r, const uint16_t t) {
+    resistance = r;
+    timestamp = t;
+  }
+
+  void set(const uint16_t r, const uint16_t t, const uint16_t tol) {
+    resistance = r;
+    timestamp = t;
+    tolerance = tol;
+  }
+
+  void setTolerance(const uint16_t t) {
+    tolerance = t;
+  }
+
+  uint16_t getResistance() const {
+    return resistance;
+  }
+
+  uint16_t getTimestamp() const {
+    return timestamp;
+  }
+
+  uint16_t getTolerance() const {
+    return tolerance;
+  }
+
+  boolean resistanceEqualsTolerance(const uint16_t expected) const
+  {
+    return ((resistance >= expected - tolerance) && (resistance <= expected + tolerance));
+  }
+
+  void operator = (const WheelKeyEvent v)  {
+    resistance = v.getResistance();
+    timestamp = v.getTimestamp();
+    tolerance = v.getTolerance();
+  }
+
+};
+
+WheelKeyEvent data;
+WheelKeyEvent dataPrev;
+
+uint16_t flag = 0;
+uint16_t eventTime = 0;
+uint16_t pauseMillis = 0;
+
+boolean between(const uint16_t value, const uint16_t a, const uint16_t b)
 {
-  if ((value >= expected - TOLERANCE) && (value <= expected + TOLERANCE)) return 1;
-  else return 0;
+  return ( (value >= a) && (value <= b) );
 }
 
 void setup()
 {
+    data.set(NOBUTTON, 0, 10);
+    dataPrev.set(NOBUTTON, 0, 10);
 }
 
 void loop()
 {
-  data=analogRead(analogPin); //читаем значение нажатой кнопки
+  data.set(analogRead(analogPin), millis());
 
+  if (!data.resistanceEqualsTolerance(dataPrev.getResistance())) {
+    dataPrev = data;
+  }
 
+  if (data.resistanceEqualsTolerance(NOBUTTON)) {
 
-  if((tolerance(data, NOBUTTON) || tolerance(data, 0)) && flag == 0) // все кнопки отпущены
-   {
-    eventTime=millis();
-   }
-
-//********************** Громкость ************************************
-  if(between(millis()-eventTime, 10, 499) && tolerance(data, VOLUP) && flag == 0) // короткое нажатие кнопки от 10 до 499 миллисекунд (изменяется здесь millis()-eventTime>10 && millis()-eventTime<499)
-   {
+      Tablet.clear();
+  } else if (data.resistanceEqualsTolerance(VOLUP)) {
     Tablet.vol_up();
-    flag = 5;
-    delay(10);
+    delay(250);
     Tablet.clear();
-   }
-
-  if(millis()-eventTime>500 && tolerance(data, VOLUP) && flag == 5 || tolerance(data, VOLUP) && flag == 3) // удержание кнопки от 500 до ? миллисекунд (изменяется здесь millis()-eventTime>500)
-   {
-    Tablet.vol_up(); // действие после удержание кнопки, Увеличить громкость каждые 0,1 секунды
-    flag = 3;
-    Tablet.clear();
-    delay(100);
-   }
-
-  if(between(millis()-eventTime, 10, 499) && tolerance(data, VOLDOWN) && flag == 0) // короткое нажатие кнопки от 10 до 499 миллисекунд (изменяется здесь millis()-eventTime>10 && millis()-eventTime<499)
-   {
+  } else if (data.resistanceEqualsTolerance(VOLDOWN)) {
     Tablet.vol_down();
-    flag = 6;
-    delay(10);
+    delay(250);
     Tablet.clear();
-   }
-
-  if(millis()-eventTime>500 && tolerance(data, VOLDOWN) && flag == 6 || tolerance(data, VOLDOWN) && flag == 3) // удержание кнопки от 500 до ? миллисекунд (изменяется здесь millis()-eventTime>500)
-   {
-    Tablet.vol_down(); // действие после удержание кнопки, Уменьшить громкость каждые 0,1 секунды
-    flag = 3;
-    Tablet.clear();
-    delay(100);
-   }
-
-//********************** AltTab + Play/pause ************************************
-
-if(between(millis()-eventTime, 10, 499) && tolerance(data, PLAY)) // короткое нажатие кнопки от 10 до 499 миллисекунд (изменяется здесь millis()-eventTime>10 && millis()-eventTime<499)
-   {
-    flag = 1;
-   }
-
-   if(tolerance(data, NOBUTTON) && flag == 1 && pauseMillis == 0) // действие после короткого нажатия кнопки, Alt+Tab
-   {
-    Tablet.play_pause();
-    flag = 0;
-    delay(10);
-    Tablet.clear();
-   }
-//********************** Next и Fast Forward ************************************
-
-  if(between(millis()-eventTime, 10, 499) && tolerance(data, NEXT)) // короткое нажатие кнопки от 10 до 499 миллисекунд (изменяется здесь millis()-eventTime>10 && millis()-eventTime<499)
-   {
-    flag = 2;
-   }
-
-  if(tolerance(data, NOBUTTON) && flag == 2) // действие после короткого нажатия кнопки, Next
-   {
+  } else if (data.resistanceEqualsTolerance(NEXT) && (data.getTimestamp() - dataPrev.getTimestamp() <= 500) ) {
     Tablet.next();
-    flag = 0;
-    delay(10);
+    delay(500);
     Tablet.clear();
-   }
-
-  if(millis()-eventTime>500 && tolerance(data, NEXT) && flag == 2 || tolerance(data, NEXT) && flag == 3) // удержание кнопки от 500 до ? миллисекунд (изменяется здесь millis()-eventTime>500)
-   {
-    Tablet.forward(); // действие после удержание кнопки, Fast Forward
-    flag = 3;
-    Tablet.clear();
-    delay(100);
-   }
-//********************** Previous и Rewind ************************************
-
-  if(between(millis()-eventTime, 10, 499) && tolerance(data, PREV)) // короткое нажатие кнопки от 10 до 499 миллисекунд (изменяется здесь millis()-eventTime>10 && millis()-eventTime<499)
-   {
-    flag = 4;
-   }
-
-  if(tolerance(data, NOBUTTON) && flag == 4) // действие после короткого нажатия кнопки, Previous
-   {
+  } else if (data.resistanceEqualsTolerance(PREV) && (data.getTimestamp() - dataPrev.getTimestamp() <= 500) ) {
     Tablet.previous();
-    flag = 0;
-    delay(10);
+    delay(500);
     Tablet.clear();
-   }
-
-  if(millis()-eventTime>500 && tolerance(data, PREV) && flag == 4 || tolerance(data, PREV) && flag == 3) // удержание кнопки от 500 до ? миллисекунд (изменяется здесь millis()-eventTime>500)
-   {
-    Tablet.rewind(); // действие после удержание кнопки, Rewind
-    flag = 3;
+} else if (data.resistanceEqualsTolerance(NEXT) && dataPrev.resistanceEqualsTolerance(NEXT) && (data.getTimestamp() - dataPrev.getTimestamp() > 500) ) {
+    Tablet.forward();
+    delay(250);
     Tablet.clear();
-    delay(100);
-   }
+  } else if (data.resistanceEqualsTolerance(PREV) && dataPrev.resistanceEqualsTolerance(PREV) && (data.getTimestamp() - dataPrev.getTimestamp() > 500) ) {
+    Tablet.rewind();
+    delay(250);
+    Tablet.clear();
+  } else if (data.resistanceEqualsTolerance(PLAY)) {
+    Tablet.play_pause();
+    delay(500);
+    Tablet.clear();
+  }
 
-  if(tolerance(data, NOBUTTON) && flag == 3 || tolerance(data, NOBUTTON) && flag == 5 || tolerance(data, NOBUTTON) && flag == 6) // действие после отпускания кнопки
-   {
-    flag = 0;
-   }
 }
